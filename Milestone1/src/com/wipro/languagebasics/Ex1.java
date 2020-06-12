eda1d9890c6dfbb9cd6d2b4f23117322fbd4eee5
pc <pc@DESKTOP-IRR0IR0>
2020-06-12 09:37:42 +0530

    A java Program that accepts two Strings as command line arguments and generate the output in the required format. Example1) If the two command line arguments are Wipro and Bangalore then the output generated should be Wipro Technologies Bangalore.

---
+++ Milestone1/src/com/wipro/languagebasics/Ex1.java
@@ -0,0 +1,10 @@
+package com.wipro.languagebasics;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		String s=String.valueOf(args[0]);
+		String s1=String.valueOf(args[1]);
+		System.out.println(s+" Technologices "+s1);
+	}
+
+}
