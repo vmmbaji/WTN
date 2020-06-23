a075764a34e20aca77e4bb8b69c8385aeba180a1
pc <pc@DESKTOP-IRR0IR0>
2020-06-23 16:12:00 +0530

    A program to accept gender ("Male" or "Female") and age from command line arguments and print the percentage of interest based on the given conditions.
    If the gender is 'Female' and age is between 1 and 58, the percentage of
    interest is 8.2%.
    
    If the gender is 'Female' and age is between 59 and 100, the percentage
    of interest is 9.2%.
    
    If the gender is 'Male' and age is between 1 and 58, the percentage of
    interest is 8.4%.
    
    If the gender is 'Male' and age is between 59 and 100, the percentage of
    interest is 10.5%.

---
+++ Milestone1/src/com/wipro/languagebasics/Ex9.java
@@ -0,0 +1,20 @@
+package com.wipro.languagebasics;
+
+public class Ex9 {
+	public static void main(String[] args) {
+		String s=String.valueOf(args[0]);
+		int age=Integer.parseInt(args[1]);
+		String s1="Male";
+		String s2="Female";
+		if(s.compareTo(s1)==0 && (age>=1 && age<=58))
+			System.out.println("The percentage of interest is 8.4%");
+		else if(s.compareTo(s1)==0 && (age>=59 && age<=100))
+			System.out.println("The percentage of interest is 10.5%");
+		else if(s.compareTo(s2)==0 && (age>=1 && age<=58))
+			System.out.println("The percentage of interest is 8.2%");
+		else
+			System.out.println("The percentage of interest is 9.2%");
+		
+	}
+
+}
