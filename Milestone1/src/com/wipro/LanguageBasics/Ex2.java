2845936aed21334b7561a57f2d153f6a1e8fbbd7
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:13:43 +0530

    a Program to accept a String as a command line argument and print a Welcome message as given below.
    Example1)
    C:\> java Sample John
    O/P Expected : Welcome John

---
+++ Milestone1/src/com/wipro/LanguageBasics/Ex2.java
@@ -0,0 +1,10 @@
+package com.wipro.LanguageBasics;
+
+public class Ex2 {
+	public static void main(String[] args)
+	{
+		String s=String.valueOf(args[0]);
+		System.out.println("Welcome "+s);
+	}
+
+}
