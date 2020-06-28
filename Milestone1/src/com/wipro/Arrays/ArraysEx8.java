d5cae004d9ebe37305bdbb88632823744d3f4f90
pc <pc@DESKTOP-IRR0IR0>
2020-06-29 00:03:23 +0530

    a program to print the sum of the elements of an array following the given below condition.
    If the array has 6 and 7 in succeeding orders, ignore the numbers
    between 6 and 7 and consider the other numbers for calculation of sum.
    
    Eg1) Array Elements - 10,3,6,1,2,7,9
    O/P: 22
    [i.e 10+3+9]
    
    Eg2) Array Elements - 7,1,2,3,6
    O/P:19
    
    Eg3) Array Elements - 1,6,4,7,9
    O/P:10

--- Milestone1/src/com/wipro/Arrays/ArraysEx8.java
+++ Milestone1/src/com/wipro/Arrays/ArraysEx8.java
@@ -2,14 +2,18 @@ package com.wipro.Arrays;
 
 public class ArraysEx8 {
 	public static void main(String[] args) {
-		int[] a={12,34,12,45,67,89};
+		int[] a= {1,6,4,7,9};
+		int s=0,fi=0,si=0;
 		for(int i=0;i<a.length;i++) {
-			for(int j=i+1;j<a.length;j++) {
-				if(a[i]==a[j])
-					a[j]=-1;
-			}
-			if(a[i]!=-1)
-				System.out.print(a[i]+" ");
+			if(a[i]==6)
+				fi=i;
+			if(a[i]==7)
+				si=i;
 		}
+		for(int i=fi;i<=si;i++)
+			a[i]=0;
+		for(int i:a)
+			s+=i;
+		System.out.println(s);
 	}
 }
