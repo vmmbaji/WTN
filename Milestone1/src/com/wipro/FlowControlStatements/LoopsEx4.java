9f912c0817ba7ec86da01b5192ce101504710cef
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:36:34 +0530

    Initialize two character variables in a program and display the characters in alphabetical order.
    Example1) if the first character is 's' and second character is 'e' then
    the output should be  e,s
    
    Example2) if the first character is 'a' and second character is 'e',
    then the output should be a,e

---
+++ Milestone1/src/com/wipro/FlowControlStatements/LoopsEx4.java
@@ -0,0 +1,12 @@
+package com.wipro.FlowControlStatements;
+
+public class LoopsEx4 {
+	public static void main(String args) {
+		char char1='s',char2='e';
+		if(char1>char2)
+			System.out.println(char2+","+char1);
+		else
+			System.out.println(char1+","+char2);
+	}
+
+}
