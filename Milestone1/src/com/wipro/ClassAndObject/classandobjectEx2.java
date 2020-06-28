631a821e38fd2f1ec3b77dfbe9b75aee245ed7b7
pc <pc@DESKTOP-IRR0IR0>
2020-06-29 00:08:24 +0530

    Create a new class called Calculator with the following methods: 1. A static method called powerInt(int num1,int num2) This method should return num1 to the power num2. 2. A static method called powerDouble(double num1,int num2). This method should return num1 to the power num2. 3. Invoke both the methods and test the functionalities. Hint: Use Math.pow(double,double) to calculate the power.

---
+++ Milestone1/src/com/wipro/ClassAndObject/classandobjectEx2.java
@@ -0,0 +1,15 @@
+package com.wipro.ClassAndObject;
+class Calculator {
+	public static int powerInt(int num1, int num2) {
+		return (int) Math.pow(num1, num2);
+	}
+	
+	public static double powerDouble(double num1, int num2) {
+		return Math.pow(num1, num2);
+	}
+}
+public class classandobjectEx2 {public static void main(String[] args) {
+	System.out.println(Calculator.powerInt(12, 2));
+	System.out.println(Calculator.powerDouble(1.5, 2));
+}
+}
