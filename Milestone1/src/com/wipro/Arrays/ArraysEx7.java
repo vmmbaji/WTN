95ba97413413640755f5db85c894d1eb42671019
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:52:03 +0530

    a program to remove the duplicate elements in an array and print the same. Example) I/P:{12,34,12,45,67,89}

---
+++ Milestone1/src/com/wipro/Arrays/ArraysEx7.java
@@ -0,0 +1,12 @@
+package com.wipro.Arrays;
+
+import java.util.Arrays;
+
+public class ArraysEx7 {
+	public static void main(String[] args) {
+		int[] a= {5,89,58,26,4,47,1,99,97,3};
+		Arrays.sort(a);
+		for(int i:a)
+			System.out.print(i+" ");
+	}
+}
