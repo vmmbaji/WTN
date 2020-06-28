403e8488b39621e00ae5d21f73f3f65d9e29891e
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:45:26 +0530

    a program to print first 5 values which are divisible by 2, 3, and 5.

---
+++ Milestone1/src/com/wipro/FlowControlStatements/LoopsEx19.java
@@ -0,0 +1,12 @@
+package com.wipro.FlowControlStatements;
+
+public class LoopsEx19 {
+	public static void main(String[] args) {
+		int n=1;
+		while(n<=5) {
+			System.out.println(30*n);
+			n++;
+		}
+		
+	}
+}
