aab73fe4cbeb3b7ed26074acdc5a6753efacb9eb
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:40:39 +0530

    a program to print even numbers between 23 and 57. Each number should be printed in a separate row.

---
+++ Milestone1/src/com/wipro/FlowControlStatements/LoopsEx11.java
@@ -0,0 +1,11 @@
+package com.wipro.FlowControlStatements;
+
+public class LoopsEx11 {
+	public static void main(String[] args) {
+		for(int i=23;i<=57;i++)
+		{
+			if(i%2==0)
+				System.out.println(i);
+		}
+	}
+}
