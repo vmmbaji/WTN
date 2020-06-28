43028cff080accfa3a1e3a44188287fd5fd35ae8
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:50:07 +0530

    Initialize an integer array with ascii values and print the corresponding character values in a single row.

---
+++ Milestone1/src/com/wipro/Arrays/ArraysEx4.java
@@ -0,0 +1,11 @@
+package com.wipro.Arrays;
+
+public class ArraysEx4 {
+	public static void main(String[] args) {
+		char[] a= {65,98,99,64,49,50,51};
+		for(char c:a) {
+			System.out.print(c);
+		}
+		
+	}
+}
