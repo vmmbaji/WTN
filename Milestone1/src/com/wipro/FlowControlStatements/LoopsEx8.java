134a0bebb8f28f986a510eb493042d9114aeee38
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:39:06 +0530

    a program to receive a color code from the user (an Alphabhet).
    The program should then print the color name, based on the color code
    given.
    
    The following are the color codes and their corresponding color names.
    R->Red, B->Blue, G->Green, O->Orange, Y->Yellow, W->White.
    
    If color code provided by the user is not valid then print "Invalid
    Code".

---
+++ Milestone1/src/com/wipro/FlowControlStatements/LoopsEx8.java
@@ -0,0 +1,24 @@
+package com.wipro.FlowControlStatements;
+
+public class LoopsEx8 {
+	public static void main(String[] args) {
+		String str=args[0];
+		char ch=str.charAt(0);
+		switch(ch) {
+		case 'R':System.out.println("Red");
+		break;
+		case 'B':System.out.println("Blue");
+		break;
+		case 'G':System.out.println("Green");
+		break;
+		case 'O':System.out.println("Orange");
+		break;
+		case 'Y':System.out.println("Yellow");
+		break;
+		case 'W':System.out.println("White");
+		break;
+		default :System.out.println("Invalid Code");
+		}
+		
+	}
+}
