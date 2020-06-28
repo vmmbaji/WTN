0c53b9a5cfb1e9c83d2c3c3819d34e503fae07e6
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:28:28 +0530

    a program to check if a given integer number is odd or even.

---
+++ Milestone1/src/com/wipro/FlowControlStatements/LoopsEx2.java
@@ -0,0 +1,12 @@
+package com.wipro.FlowControlStatements;
+
+public class LoopsEx2 {
+	public static void main(String args[]) {
+		int x=Integer.parseInt(args[0]);
+		if(x%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
