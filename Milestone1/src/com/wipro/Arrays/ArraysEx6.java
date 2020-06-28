37aa46b96ebcef01bc6cac7c078840bf9d098e81
pc <pc@DESKTOP-IRR0IR0>
2020-06-29 00:02:28 +0530

    a program to initialize an array and print them in a sorted order.

--- Milestone1/src/com/wipro/Arrays/ArraysEx6.java
+++ Milestone1/src/com/wipro/Arrays/ArraysEx6.java
@@ -1,4 +1,12 @@
 package com.wipro.Arrays;
 
+import java.util.Arrays;
+
 public class ArraysEx6 {
+	public static void main(String[] args) {
+		int[] a= {5,89,58,26,4,47,1,99,97,3};
+		Arrays.sort(a);
+		for(int i:a)
+			System.out.print(i+" ");
+	}
 }
