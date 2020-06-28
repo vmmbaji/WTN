ce7f0971fbc49684344dc2a8b42a591f286c8564
pc <pc@DESKTOP-IRR0IR0>
2020-06-28 23:29:01 +0530

    a program to check if the program has received command line arguments or not.
    If the program has not received arguments then print "No Values", else
    print all the values in a single line separated by ,(comma)
    
    Example1) java Example
    O/P: No values
    
    Example2) java Example Mumbai Bangalore
    O/P: Mumbai,Bangalore
    
    [Hint: You can use length property of an array to check its length]

---
+++ Milestone1/src/com/wipro/FlowControlStatements/LoopsEx3.java
@@ -0,0 +1,22 @@
+package com.wipro.FlowControlStatements;
+
+public class LoopsEx3 {
+	public static void main(String[] args) {
+		if(args.length==0)
+		{
+			System.out.println("No value");
+		}
+		else
+		{
+			int n=0;
+			for(String s:args)
+			{
+				System.out.print(s);
+				if(n<args.length-1)
+					System.out.print(",");
+				n++;
+			}
+		}
+	}
+
+}
